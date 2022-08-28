e4d4e976b321c272729dae2b80ca7e409420767e
Guilherme Mendes de Olveira <guilhermemendesdeoliveira94@gmail.com>
2022-08-28 15:54:00 -0300

    Add pagina Cadastro.html, Refatorando arquivo CSS, efeitos na pagina index.html, iniciando arquivo JS, exclusão das paginas arraial.html, foto.html, mato_grosso.html, Porto_Alegre.html e Rio_de_Janeiro.html.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+function rolarPacotes(){
+    const Pacotes = document.getElementById('pacotes')
+    Pacotes.scrollIntoView();
+}
+function rolarInter(){
+    const Inter = document.getElementById('inters')
+    Inter.scrollIntoView();
+}
+function rolarNacio(){
+    const Nacio = document.getElementById('nacio')
+    Nacio.scrollIntoView();
+}
